99f6e650cfa57dc3ab0babc3b4e7c952c924d950
Kirsi <mushi88@users.noreply.github.com>
2017-05-11 11:06:30 +1000

    Update Asset Creater.py

--- Tools/Asset Creater.py
+++ Tools/Asset Creater.py
@@ -1,1 +1,16 @@
+from tkinter import *
 
+class Window(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master=master
+        self.setup()
+        
+    def setup(self):
+        self.master.title("Asset Creater")
+        
+        self.text=Text(self)
+        
+root=Tk()
+App=Window(root)
+root.mainloop()
