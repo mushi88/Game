8d2674e7126e3e195d5281318f9162f656916a2a
Kirsi <mushi88@users.noreply.github.com>
2017-05-11 11:32:01 +1000

    Update Asset Creater.py

--- Tools/Asset Creater.py
+++ Tools/Asset Creater.py
@@ -10,6 +10,20 @@ class Window(Frame):
         self.master.title("Asset Creater")
         
         self.text=Text(self)
+        self.text.pack()
+        
+        self.menu=Menu(self)
+        self.fmenu=Menu(self.menu)
+        self.master.config(menu=self.menu)
+        self.menu.add_cascade(label="File", menu=fmenu)
+        
+        self.fmenu.add_command(label="New", command=newfile)
+        self.fmenu.add_command(label="Open File", command=openfile)
+        self.fmenu.add_command(label="Save", command=savefile)
+        self.fmenu.add_separator()
+        self.fmenu.add_command(label="Exit", command=exitapp)
+        
+    def newfile(self):
         
 root=Tk()
 App=Window(root)
